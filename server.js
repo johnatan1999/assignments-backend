358a1a7dd900a25755ec95349ed60b0904db3bf5
johnatan <nathanjoh143@gmail.com>
2021-03-10 20:06:17 +0400

    change mangodb uri

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ mongoose.Promise = global.Promise;
 
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
 //const uri = 'mongodb+srv://mb:P7zM3VePm0caWA1L@cluster0.zqtee.mongodb.net/assignments?retryWrites=true&w=majority';
-const uri='mongodb+srv://mb:toto@cluster0.xtr0u.mongodb.net/assignments?retryWrites=true&w=majority';
+const uri='mongodb+srv://joh:foot@cluster0.wqqnt.mongodb.net/assignments?retryWrites=true&w=majority';
 
 const options = {
   useNewUrlParser: true,
