383edaf10c3218a280b1cc7af0fd1b471a502407
johnatan <nathanjoh143@gmail.com>
2021-04-15 13:34:42 +0400

    s

--- server.js
+++ server.js
@@ -40,11 +40,10 @@ mongoose.connect(uri, options)
 // app.use(cors());
 app.options('*', cors())
 app.use((req, res, next) =>  {
-  console.log(req.headers)
-  res.append("Access-Control-Allow-Origin", '*');
+  res.setHeader("Access-Control-Allow-Origin", '*');
   // res.header("Access-Control-Allow-Origin", "http://localhost:4200/, https://assignments-frontend.herokuapp.com");
-  res.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, UserId, Authorization");
-  res.append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, UserId, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
